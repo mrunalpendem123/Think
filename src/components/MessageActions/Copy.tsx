59b22a5fa776d12523d0069a6f13d4cb574c238a
mrunal pendem <mrunalpendem@mrunals-MacBook-Pro.local>
2025-11-25 22:39:35 +0530

    Update Copy component logic to handle text prop

--- src/components/MessageActions/Copy.tsx
+++ src/components/MessageActions/Copy.tsx
@@ -17,7 +17,7 @@ const Copy = ({
   return (
     <button
       onClick={() => {
-        const contentToCopy = `${initialMessage}${section?.sourceMessage?.sources && section.sourceMessage.sources.length > 0 && `\n\nCitations:\n${section.sourceMessage.sources?.map((source: any, i: any) => `[${i + 1}] ${source.metadata.url}`).join(`\n`)}`}`;
+        const contentToCopy = text || `${initialMessage || ''}${section?.sourceMessage?.sources && section.sourceMessage.sources.length > 0 ? `\n\nCitations:\n${section.sourceMessage.sources?.map((source: any, i: any) => `[${i + 1}] ${source.metadata.url}`).join(`\n`)}` : ''}`;
         navigator.clipboard.writeText(contentToCopy);
         setCopied(true);
         setTimeout(() => setCopied(false), 1000);
